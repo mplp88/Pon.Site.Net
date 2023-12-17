9a3cd6c02cbdfb2b04df04dd4628ac6a057f0b95
Martín Ponce <martinp88@gmail.com>
2023-12-17 02:57:12 -0300

    Add Web Project

---
+++ Pon.Site.Net.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pon.Site.Net.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
